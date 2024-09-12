efe54e428b8737c74bdd64f8c2d4e3c9fa3e5415
Mujahid Hussain <hmujahid08@gmail.com>
2024-09-12 19:15:58 +0500

    React Native setup

--- client/App.js
+++ client/App.js
@@ -1,10 +1,10 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {View, Text} from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
     <View>
-      <Text>Hello World </Text>
+      <Text>App</Text>
     </View>
   );
 };

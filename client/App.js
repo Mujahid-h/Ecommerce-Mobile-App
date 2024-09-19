02341fd65d161aff7d1d96b84dea8afacae225fe
Mujahid Hussain <hmujahid08@gmail.com>
2024-09-19 18:19:12 +0500

    Setup

--- client/App.js
+++ client/App.js
@@ -1,12 +1,20 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

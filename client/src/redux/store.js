6236cd0669db153225e69f6a3a82c07622727e60
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-01 22:20:06 +0500

    "Added productsSlice to store reducer in configureStore"

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsSlice from "./slices/productsSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    // Action will render here
+    products: productsSlice,
+  },
 });
 
 export default store;

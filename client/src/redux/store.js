179ae9d29aca9ce7b2ab3e25d9445cc211376a20
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-07 22:50:36 +0500

    "Added WishlistSlice to store reducer in store.js"

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsSlice from "./slices/productsSlice";
+import WishlistSlice from "./slices/WishlistSlice";
 
 const store = configureStore({
   reducer: {
     // Action will render here
     product: productsSlice,
+    wishlist: WishlistSlice,
   },
 });
 

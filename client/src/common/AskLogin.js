962c66e7bbaa34beae5d3ab911765542b8bb2d87
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-12 23:54:00 +0500

    Add a common file for modal

---
+++ client/src/common/AskLogin.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AskLogin = () => {
+  return (
+    <View>
+      <Text>AskLogin</Text>
+    </View>
+  );
+};
+
+export default AskLogin;

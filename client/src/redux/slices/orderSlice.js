0be695fecf257ec5e98292a051cfc1991b97c138
Mujahid Hussain <hmujahid08@gmail.com>
2024-10-25 22:24:05 +0500

    Added orderSLice

---
+++ client/src/redux/slices/orderSlice.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const orderSlice = () => {
+  return (
+    <View>
+      <Text>orderSlice</Text>
+    </View>
+  );
+};
+
+export default orderSlice;
+
+const styles = StyleSheet.create({});
